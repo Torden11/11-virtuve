97a0254f613bc1358a8481afda939fc157466cc7
Sergej <sergej11@yahoo.com>
2022-07-14 20:18:51 +0300

    add/remove class, innerText, style, HTML insert into js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
 import { Kaitlente } from "./Kaitlente.js";
 
-const kaitlentciuKiekis = 5;
+const kaitlentciuKiekis = 4;
 
 // h1 -> Virtuve
 // h1 -> Dirbtuve (5)
 
-const titleDOM = document.querySelector('.title');
-console.log(titleDOM);
+// const h1DOM = document.querySelector('h1');
+// const h1DOM = document.querySelectorAll();
+//h1DOM, titleDOM sugalvoti pavadinimai
+//const skyleDOM = document.querySelector('.kaitlente.skyle:nth-child(3)');
+//. title siuo atveju kreipimasis i klase
 
-// for (let i = 1; i <= kaitlentciuKiekis; i++) {
-//     const irenginys = new Kaitlente(i);
-//     irenginys.render();
-// }
+const titleDOM = document.querySelector('.title');
+titleDOM.innerText = `Dirbtuve (${kaitlentciuKiekis})`;
+console.log(kaitlentciuKiekis);
+
+//console.log(titleDOM);
+//console.log(titleDOM.innerText);
+//console.log(titleDOM.textContent);
+
+//Su .innerText pagalba galima pakeisti h1 reiksme.
+
+//title tampa BOLD: lyginis skyliu skaicius
+
+
+if (kaitlentciuKiekis % 2 ===0 ) {
+    titleDOM.classList.add('bold');
+    
+}
+console.log(titleDOM.classList);
+
+//title tampa RED: bent viena skyle yra karsta
+//su .classList.add galima prideti (arba .remove atimti) klase ir atitinkamai pakeisti elemento isvaizda tam tikromis salygomis
+//titileDOM.style.backgroundColor='yellow' arba titleDOM.style.['background-color']='yellow'
+
+if(document.querySelector('.karsta')); {
+    titleDOM.classList.add('red');
+//    titleDOM.style.backgroundColor='yellow';
+}
+
+document.querySelector('main').textContent = '';
+for (let i = 1; i <= kaitlentciuKiekis; i++) {
+    const irenginys = new Kaitlente(i);
+     irenginys.render();
+ }
